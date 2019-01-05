a19e07b953722671577ab4613cfd0c23d2f5f6a4
Krzysztof Surulo <krzysztof.surulo@gmail.com>
2019-01-05 21:10:28 +0000

    Add p2 production

--- src/main/java/productions/P2.java
+++ src/main/java/productions/P2.java
@@ -1,10 +1,22 @@
 package productions;
 
+import common.Geom;
+import common.Label;
+import common.Type;
+import org.graphstream.algorithm.AStar;
+import org.graphstream.graph.Edge;
 import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
+import org.graphstream.graph.Path;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import static common.Label.V;
 
 @Service
 public class P2 {
@@ -15,4 +27,94 @@ public class P2 {
     public Graph prepareTestGraph(BufferedImage img) {
         return p1.run(img);
     }
+
+    private boolean verifyNodes(Graph graph, Node nodeI) {
+        if (nodeI == null || !Boolean.getBoolean(nodeI.getAttribute("break"))) {
+            return false;
+        }
+        Iterator<Node> iterator = nodeI.getNeighborNodeIterator();
+        ArrayList<Node> neighbours = new ArrayList<>();
+
+        while (iterator.hasNext()) {
+            neighbours.add(iterator.next());
+        }
+
+        if (neighbours.size() != 4) {
+            return false;
+        }
+        return true;
+    }
+
+    public Graph run(Graph graph, BufferedImage img, Node nodeI) {
+
+        if (verifyNodes(graph, nodeI)) {
+            Node v = replaceI(graph, nodeI);
+            Node fn = createHyperEdge(graph, Label.FN);
+            Node fe = createHyperEdge(graph, Label.FE);
+            Node fw = createHyperEdge(graph, Label.FW);
+            Node fs = createHyperEdge(graph, Label.FS);
+
+            addEdge(graph, v, fn);
+            addEdge(graph, v, fe);
+            addEdge(graph, v, fw);
+            addEdge(graph, v, fs);
+
+
+        }
+        return graph;
+    }
+
+    private Node replaceI(Graph graph, Node nodeI) {
+        Node v = createVertexToReplaceI(graph, nodeI);
+        for (Iterator<Node> it = nodeI.getNeighborNodeIterator(); it.hasNext(); ) {
+            Node neighbour = it.next();
+            // graph.removeEdge(nodeI, neighbour); not needed, as we remove the I node later
+            Node newI = createHyperEdge(graph, Label.I);
+            addEdge(graph, v, newI);
+            addEdge(graph, newI, neighbour);
+        }
+        graph.removeNode(nodeI);
+        return v;
+    }
+
+    private Node createVertexToReplaceI(Graph graph, Node nodeI) {
+        return addNode(graph, Integer.toString(graph.getNodeCount() + 1), nodeI.getAttribute("geom"),
+                Type.VERTEX, V, false, nodeI.getAttribute("rgb"));
+    }
+
+    private Node createHyperEdge(Graph graph, Label label) {
+        return addNode(graph, Integer.toString(graph.getNodeCount() + 1),
+                Type.HYPEREDGE, label, false);
+    }
+
+
+    private Color getColor(BufferedImage img, Geom geom) {
+        return new Color(img.getRGB(geom.getX(), geom.getY()));
+    }
+
+    private Node addNode(Graph graph, String name, Type type, Label label, boolean isBreak) {
+        Node node = graph.addNode(name);
+        node.setAttribute("type", type);
+        node.setAttribute("label", label);
+        node.setAttribute("break", isBreak);
+        return node;
+    }
+
+
+    private Node addNode(Graph graph, String name, Geom geom, Type type, Label label, boolean isBreak, Color rgb) {
+        Node node = graph.addNode(name);
+        node.setAttribute("geom", geom);
+        node.setAttribute("type", type);
+        node.setAttribute("label", label);
+        node.setAttribute("break", isBreak);
+        node.setAttribute("rgb", rgb);
+        return node;
+    }
+
+    private void addEdge(Graph graph, Node source, Node target) {
+        String name = source.getId() + "-" + target.getId();
+        Edge edge = graph.addEdge(name, source, target);
+    }
+
+
 }
