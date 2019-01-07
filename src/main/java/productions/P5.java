757925efa3f1d2d4da5381f0bfc786aadaeeac2d
KCieslik <kmacieslik@gmail.com>
2019-01-07 21:48:00 +0100

    Circular dependency fixes.

--- src/main/java/productions/P5.java
+++ src/main/java/productions/P5.java
@@ -3,8 +3,6 @@ package productions;
 import common.Type;
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.Node;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 import java.awt.image.BufferedImage;
@@ -14,13 +12,12 @@ public class P5 {
 
     private final P6 p6;
 
-    @Autowired
-    public P5(@Lazy P6 p6) {
-        this.p6 = p6;
+    public P5() {
+        this.p6 = new P6(this);
     }
 
     public Graph run(Graph graph, BufferedImage img, Node node) {
-        if (verify(graph, img, node)) return graph;
+        if (!verify(graph, img, node)) return graph;
         node.setAttribute("break", true);
         return p6.run(graph, img, node);
     }
