9716755aa0b3ad0643438b4100646227742da432
annasla95 <annaslabuszewska@gmail.com>
2019-01-08 21:59:27 +0100

    z12 bitmap aproximator draft

---
+++ src/main/java/approximation/BitmapAproximator.java
@@ -0,0 +1,17 @@
+package approximation;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class BitmapAproximator {
+
+    public BufferedImage bitmap;
+
+    public int[][] APPROX_R;
+    public int[][] APPROX_G;
+    public int[][] APPROX_B;
+
+    public void run(Color rgb1, Color rgb2, Color rgb3, Color rgb4, int x1, int y1, int x2, int y2) {
+
+    }
+}
