cf0a29491328a8f2d546a8684118eb68ba8110a5
Damian Sosnowski <sosnowski.damian@gmail.com>
2019-01-10 01:03:53 +0100

    Squashed

--- src/main/java/parser/Parser.java
+++ src/main/java/parser/Parser.java
@@ -2,7 +2,6 @@ package parser;
 
 import common.Geom;
 import common.Label;
-import org.graphstream.graph.Edge;
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.Node;
 import org.springframework.context.ApplicationContext;
@@ -10,7 +9,6 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.ComponentScan;
 import productions.P1;
 import productions.P2;
-import productions.P3;
 import productions.P4;
 
 import javax.imageio.ImageIO;
@@ -43,52 +41,26 @@ public class Parser {
         ClassLoader cl = Thread.currentThread().getContextClassLoader();
         BufferedImage img = ImageIO.read(Objects.requireNonNull(cl.getResourceAsStream("colors.jpg")));
 
-        int x1;
-        int y1;
-        int x2;
-        int y2;
-
         switch(productionNumber){
             case 1:
                 P1 p1 = context.getBean(P1.class);
-                return p1.run(coordinates, rgb);
+                return p1.run(img);
             case 2:
                 P2 p2 = context.getBean(P2.class);
-                x1 = coordinates[0];
-                y1 = coordinates[1];
-                x2 = coordinates[2];
-                y2 = coordinates[3];
+                int x1 = coordinates[0];
+                int y1 = coordinates[1];
+                int x2 = coordinates[2];
+                int y2 = coordinates[3];
 
                 Node nodeI = graph.getNodeSet().stream()
                         .filter(node -> node.hasAttribute("label") &&
                                         node.<Label>getAttribute("label").equals(Label.I) &&
-                                        node.hasAttribute("geom") &&
                                         node.<Geom>getAttribute("geom").getX() == (x1 + x2) / 2 &&
                                         node.<Geom>getAttribute("geom").getY() == (y1 + y2) / 2
                         ).findFirst().get();
                 return p2.run(graph, img, nodeI);
             case 3:
-                x1 = coordinates[0];
-                x2 = coordinates[1];
-                y1 = coordinates[2];
-
-                P3 p3 = context.getBean(P3.class);
-                Graph testGraph = p3.prepareTestGraph(img);
-                Edge edge1 = graph.getEdgeSet().stream()
-                        .filter(edge -> edge.getNode0().hasAttribute("geom") &&
-                                        edge.getNode1().hasAttribute("geom") &&
-                                        edge.getNode0().<Geom>getAttribute("geom").getX() == x1 &&
-                                        edge.getNode0().<Geom>getAttribute("geom").getY() == y1 &&
-                                        edge.getNode1().<Geom>getAttribute("geom").getX() == x2 &&
-                                        edge.getNode1().<Geom>getAttribute("geom").getY() == y1)
-                        .findFirst().get();
-                Node f1 = graph.getNodeSet().stream()
-                        .filter(node -> node.hasAttribute("label") &&
-                                node.<Label>getAttribute("label").equals(Label.FN) &&
-                                node.<Geom>getAttribute("geom").getX() == (x1 + x2) / 2
-                        ).findFirst().get();
-
-                return p3.run(testGraph, img, edge1, f1);
+                break;
             case 4:
                 P4 p4 = context.getBean(P4.class);
 
@@ -97,16 +69,16 @@ public class Parser {
                 x2 = coordinates[2];
                 y2 = coordinates[3];
 
+                // Extracting input nodes for P4 test
+//                Node nodeFN = p4.getNodeByLabel(graph, Label.FN);
                 Node nodeFN = graph.getNodeSet().stream()
                         .filter(node -> node.<Label>getAttribute("label").equals(Label.FN) &&
-                                        node.hasAttribute("geom") &&
                                         node.<Geom>getAttribute("geom").getX() == (x1 + x2) / 2 &&
                                         node.<Geom>getAttribute("geom").getY() == (y1 + y2) / 2)
                         .findFirst().get();
 
                 Node nodeFW = graph.getNodeSet().stream()
                         .filter(node ->
-                                node.hasAttribute("geom") &&
                                 node.<Geom>getAttribute("geom").getX() == x1 &&
                                 node.<Geom>getAttribute("geom").getY() == (y1 + y2) / 2
                         ).flatMap(this::neighourStream)
@@ -115,12 +87,13 @@ public class Parser {
 
                 Node nodeFE = graph.getNodeSet().stream()
                         .filter(node ->
-                                node.hasAttribute("geom") &&
                                 node.<Geom>getAttribute("geom").getX() == x2 &&
-                                node.<Geom>getAttribute("geom").getY() == (y1 + y2) / 2
+                                        node.<Geom>getAttribute("geom").getY() == (y1 + y2) / 2
                         ).flatMap(this::neighourStream)
                         .filter(node -> node.<Label>getAttribute("label").equals(Label.FE))
                         .findFirst().get();
+
+                // Run P4 production
 
                 return p4.run(graph, img, nodeFN, nodeFW, nodeFE);
             default:
@@ -148,7 +121,6 @@ public class Parser {
                     rgb = null;
                 graph = runProduction(productionNumber, coordinates, rgb, graph);
             }
-            graph.display();
 
         } catch (Exception e) {
             e.printStackTrace();
