ffc628a362f32bf32a13f7a5850ea06ed820c6db
dsuder <dsuder@agh.edu.pl>
2019-01-12 11:31:38 +0100

    Refactor, use real implementation of Alg12 (BitmapApproximator)

--- src/main/java/productions/Alg18.java
+++ src/main/java/productions/Alg18.java
@@ -1,6 +1,6 @@
 package productions;
 
-import com.sun.istack.internal.NotNull;
+import approximation.BitmapAproximator;
 import common.Geom;
 import common.Type;
 import org.springframework.stereotype.Service;
@@ -13,14 +13,6 @@ import java.util.TreeSet;
 
 @Service
 public class Alg18 {
-    // petla po wierzcholkach w grafie
-    // dla kazdego hyperedge
-    // znajdz jego sasiadow
-    // znajdz max_x, max_y, min_x, min_y
-    // wywolaj funkcje
-    // (x1,y1),(x2,y2) r1,r2,r3,r4, g1,g2,g3,g4, b1,b2,b3,b4
-    // a po naszemu
-
     public Graph run(Graph graph) {
 
         Geom geom;
@@ -40,47 +32,28 @@ public class Alg18 {
                 int minY = Collections.min(y);
                 int maxY = Collections.max(y);
 
-                int r1 = 0, r2 = 0, r3 = 0, r4 = 0;
-                int b1 = 0, b2 = 0, b3 = 0, b4 = 0;
-                int g1 = 0, g2 = 0, g3 = 0, g4 = 0;
-
                 Geom tmpGeom1 = new Geom(maxX, maxY);
                 Geom tmpGeom2 = new Geom(maxX, minY);
                 Geom tmpGeom3 = new Geom(minX, maxY);
                 Geom tmpGeom4 = new Geom(minX, minY);
 
-                if(graph.getNodeSet().stream().filter(n -> n.getAttribute("geom").equals(tmpGeom1)).count() == 1) {
-                    Color rgb = graph.getNodeSet().stream().filter(n -> n.getAttribute("geom").equals(tmpGeom1)).findAny().get().getAttribute("rgb");
-                    r1 = rgb.getRed();
-                    b1 = rgb.getBlue();
-                    g1 = rgb.getGreen();
-                }
+                Color col1 = getColorFromGeom(graph, tmpGeom1);
+                Color col2 = getColorFromGeom(graph, tmpGeom2);
+                Color col3 = getColorFromGeom(graph, tmpGeom3);
+                Color col4 = getColorFromGeom(graph, tmpGeom4);
 
-                if(graph.getNodeSet().stream().filter(n -> n.getAttribute("geom").equals(tmpGeom2)).count() == 1) {
-                    Color rgb = graph.getNodeSet().stream().filter(n -> n.getAttribute("geom").equals(tmpGeom2)).findAny().get().getAttribute("rgb");
-                    r2 = rgb.getRed();
-                    b2 = rgb.getBlue();
-                    g2 = rgb.getGreen();
-                }
-
-                if(graph.getNodeSet().stream().filter(n -> n.getAttribute("geom").equals(tmpGeom3)).count() == 1) {
-                    Color rgb = graph.getNodeSet().stream().filter(n -> n.getAttribute("geom").equals(tmpGeom3)).findAny().get().getAttribute("rgb");
-                    r3 = rgb.getRed();
-                    b3 = rgb.getBlue();
-                    g3 = rgb.getGreen();
-                }
-
-                if(graph.getNodeSet().stream().filter(n -> n.getAttribute("geom").equals(tmpGeom4)).count() == 1) {
-                    Color rgb = graph.getNodeSet().stream().filter(n -> n.getAttribute("geom").equals(tmpGeom4)).findAny().get().getAttribute("rgb");
-                    r4 = rgb.getRed();
-                    b4 = rgb.getBlue();
-                    g4 = rgb.getGreen();
-                }
-
-                //todo: alg12(minX, minY, maxX, maxY, r1, r2, r3, r4,  b1, b2, b3, b4, g1, g2, g3, g4);
+                BitmapAproximator ba = new BitmapAproximator();
+                ba.run(col1, col2, col3, col4, minX, minY, maxX, maxY);
             }
         }
 
         return graph;
+    }
+
+    private Color getColorFromGeom(Graph graph, Geom geom) {
+        Color color = new Color(0,0,0);
+        if(graph.getNodeSet().stream().filter(n -> n.getAttribute("geom").equals(geom)).count() == 1)
+            color = graph.getNodeSet().stream().filter(n -> n.getAttribute("geom").equals(geom)).findAny().get().getAttribute("rgb");
+        return color;
     }
 }
