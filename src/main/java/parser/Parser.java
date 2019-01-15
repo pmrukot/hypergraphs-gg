8c61d5e0be401c148792ac0844a3a67380e81b87
Damian Sosnowski <sosnowski.damian@gmail.com>
2019-01-15 22:04:02 +0100

    Changing bitmap points with RGB values

--- src/main/java/parser/Parser.java
+++ src/main/java/parser/Parser.java
@@ -14,6 +14,7 @@ import productions.P3;
 import productions.P4;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -26,6 +27,10 @@ public class Parser {
     int[] parseNumbersInLine(String line){
         String delimiterRegex = "[ ]*,[ ]*";
         return Arrays.stream(line.split(delimiterRegex)).mapToInt(Integer::parseInt).toArray();
+    }
+
+    void setColor(BufferedImage img, Geom geom, Color rgb) {
+        img.setRGB(geom.getX(), geom.getY(), rgb.getRGB());
     }
 
     private Stream<Node> neighourStream(Node node) {
@@ -49,19 +54,38 @@ public class Parser {
                 .findFirst().get();
     }
 
-    Graph runProduction(int productionNumber, int[] coordinates, int[] rgb, Graph graph) throws IOException {
+    Graph runProduction(int productionNumber, int[] coordinates, int[] rgb, Graph graph, BufferedImage img) throws IOException {
         ApplicationContext context = new AnnotationConfigApplicationContext(Parser.class);
-        ClassLoader cl = Thread.currentThread().getContextClassLoader();
-        BufferedImage img = ImageIO.read(Objects.requireNonNull(cl.getResourceAsStream("colors.jpg")));
 
         int x1;
         int y1;
         int x2;
         int y2;
 
+        Color pointRGB;
+        Geom pointGeom;
+
         switch(productionNumber){
             case 1:
                 P1 p1 = context.getBean(P1.class);
+                int width = img.getWidth();
+                int height = img.getHeight();
+
+                Geom topLeft = new Geom(0,height-1);
+                Geom topRight = new Geom(width - 1,height -1);
+                Geom bottomLeft = new Geom(0,0);
+                Geom bottomRight = new Geom(width -1,0);
+
+                Color topLeftRGB = new Color(rgb[0], rgb[1], rgb[2]);
+                Color topRightRGB = new Color(rgb[3], rgb[4], rgb[5]);
+                Color bottomLeftRGB = new Color(rgb[6], rgb[7], rgb[8]);
+                Color bottomRightRGB = new Color(rgb[9], rgb[10], rgb[11]);
+
+                setColor(img, topLeft, topLeftRGB);
+                setColor(img, topRight, topRightRGB);
+                setColor(img, bottomLeft, bottomLeftRGB);
+                setColor(img, bottomRight, bottomRightRGB);
+
                 return p1.run(img);
             case 2:
                 P2 p2 = context.getBean(P2.class);
@@ -69,6 +93,10 @@ public class Parser {
                 y1 = coordinates[1];
                 x2 = coordinates[2];
                 y2 = coordinates[3];
+
+                pointRGB = new Color(rgb[0], rgb[1], rgb[2]);
+                pointGeom = new Geom((x1+x2)/2, (y1+y2)/2);
+                setColor(img, pointGeom, pointRGB);
 
                 Node nodeI = graph.getNodeSet().stream()
                         .filter(node -> node.hasAttribute("label") &&
@@ -82,6 +110,10 @@ public class Parser {
                 x1 = coordinates[0];
                 x2 = coordinates[1];
                 y1 = coordinates[2];
+
+                pointRGB = new Color(rgb[0], rgb[1], rgb[2]);
+                pointGeom = new Geom((x1+x2)/2, y1);
+                setColor(img, pointGeom, pointRGB);
 
                 P3 p3 = context.getBean(P3.class);
                 Graph testGraph = p3.prepareTestGraph(img);
@@ -102,6 +134,10 @@ public class Parser {
                 x2 = coordinates[2];
                 y2 = coordinates[3];
 
+                pointRGB = new Color(rgb[0], rgb[1], rgb[2]);
+                pointGeom = new Geom((x1+x2)/2, y1);
+                setColor(img, pointGeom, pointRGB);
+
                 Node nodeFN = graph.getNodeSet().stream()
                         .filter(node -> node.<Label>getAttribute("label").equals(Label.FN) &&
                                         node.hasAttribute("geom") &&
@@ -120,9 +156,12 @@ public class Parser {
         return null;
     }
 
-    public void parseFileAndRunProduction(String pathname) {
+    public void parseFileAndRunProduction(String pathname) throws IOException{
         Scanner scanner;
         ClassLoader classLoader = getClass().getClassLoader();
+
+        ClassLoader cl = Thread.currentThread().getContextClassLoader();
+        BufferedImage img = ImageIO.read(Objects.requireNonNull(cl.getResourceAsStream("colors.jpg")));
 
         File f = new File(Objects.requireNonNull(classLoader.getResource(pathname)).getFile());
         try {
@@ -137,7 +176,7 @@ public class Parser {
                     rgb = parseNumbersInLine(scanner.nextLine());
                 else
                     rgb = null;
-                graph = runProduction(productionNumber, coordinates, rgb, graph);
+                graph = runProduction(productionNumber, coordinates, rgb, graph, img);
             }
             graph.display();
 
