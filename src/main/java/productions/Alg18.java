dc4a9ecc3e2f490642ab8d0ccf14e749f295cdd9
beata <beata.baran7.03@gmail.com>
2019-01-17 11:45:00 +0100

    p1: fixes in alg18

--- src/main/java/productions/Alg18.java
+++ src/main/java/productions/Alg18.java
@@ -3,16 +3,26 @@ package productions;
 import approximation.BitmapAproximator;
 import common.Geom;
 import common.Type;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.graphstream.graph.*;
 
 import java.awt.*;
 import java.util.Collections;
+import java.util.Optional;
 import java.util.Set;
 import java.util.TreeSet;
 
 @Service
 public class Alg18 {
+
+    private BitmapAproximator ba;
+
+    @Autowired
+    public Alg18(BitmapAproximator ba) {
+        this.ba = ba;
+    }
+
     public Graph run(Graph graph) {
 
         Geom geom;
@@ -37,12 +47,11 @@ public class Alg18 {
                 Geom tmpGeom3 = new Geom(minX, maxY);
                 Geom tmpGeom4 = new Geom(minX, minY);
 
-                Color col1 = getColorFromGeom(graph, tmpGeom1);
-                Color col2 = getColorFromGeom(graph, tmpGeom2);
-                Color col3 = getColorFromGeom(graph, tmpGeom3);
-                Color col4 = getColorFromGeom(graph, tmpGeom4);
+                Optional<Color> col1 = getColorFromGeom(graph, tmpGeom1);
+                Optional<Color> col2 = getColorFromGeom(graph, tmpGeom2);
+                Optional<Color> col3 = getColorFromGeom(graph, tmpGeom3);
+                Optional<Color> col4 = getColorFromGeom(graph, tmpGeom4);
 
-                BitmapAproximator ba = new BitmapAproximator();
                 ba.run(col1, col2, col3, col4, minX, minY, maxX, maxY);
             }
         }
@@ -50,10 +59,12 @@ public class Alg18 {
         return graph;
     }
 
-    private Color getColorFromGeom(Graph graph, Geom geom) {
+    private Optional<Color> getColorFromGeom(Graph graph, Geom geom) {
         Color color = new Color(0,0,0);
-        if(graph.getNodeSet().stream().filter(n -> n.getAttribute("geom").equals(geom)).count() == 1)
-            color = graph.getNodeSet().stream().filter(n -> n.getAttribute("geom").equals(geom)).findAny().get().getAttribute("rgb");
-        return color;
+        if(graph.getNodeSet().stream().filter(n -> n.getAttribute("geom").equals(geom)).count() == 1) {
+            return Optional.of(graph.getNodeSet().stream().filter(n -> n.getAttribute("geom").equals(geom)).findAny().get().getAttribute("rgb"));
+        } else {
+            return Optional.empty();
+        }
     }
 }
